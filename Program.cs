9f50dc4ee348baeb672fb100134dd2e1871aee32
Frank Iero <User@LAPTOP-E5V8F2P7>
2023-04-08 09:47:56 +1000

    C# task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] Numbers = input.Replace(".",",").Split(' ');
+        double[] vector = new double[Numbers.Length];
+        for (int i = 0; i < Numbers.Length; i++)
+        {
+            double num = double.Parse(Numbers[i]);
+            vector[i] = num;
+        }
+
+        double answer = 0;
+
+        foreach (double num in vector)
+        {
+            answer += num * num;
+        }
+        answer = Math.Sqrt(answer);
+        string answerStr = Convert.ToString(answer).Replace(",", ".");
+
+
+        Console.WriteLine(answerStr);
+    }
+}
